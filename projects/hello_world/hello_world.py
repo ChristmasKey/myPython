f17377872cea413675288bae67323e63f97d134d
丁佳男 <jiananst@qq.com>
2025-04-08 01:52:57 +0800

    查找列表元素

--- projects/hello_world/hello_world.py
+++ projects/hello_world/hello_world.py
@@ -26,3 +26,40 @@ print(list1)
 # insert()：在列表中插入元素
 list1.insert(0, 0)
 print(list1)
+
+"""
+修改列表元素：直接通过下标进行修改
+"""
+list2 = [1, 2, 3, 4, 5]
+list2[0] = 0
+print(list2)
+
+"""
+查找列表元素：
+in：判断指定元素是否在列表中，如果存在就返回True，否则返回False；
+not in：判断指定元素是否存在列表中，如果不存在就返回True，否则返回False；
+index()：返回指定元素在列表中第一次出现的索引，如果元素不存在，则引发ValueError；
+count()：返回指定元素在列表中出现的次数；
+"""
+list3 = ["python", "java", "c++", "php", "c#"]
+print("python" in list3)
+print("c" not in list3)
+
+# 案例：记录用户输入的name，如果重复，提示重复；如果未重复，则添加到列表中
+# name_list = []
+# while "exit" not in name_list:
+#   name = input("请输入姓名：")
+#   if name in name_list:
+#     print("姓名重复")
+#   else:
+#     name_list.append(name)
+#     print("添加成功")
+#     print(name_list)
+
+list4 = ["aa", "bb", "cc", "aa", "dd"]
+print(list4.index("dd"))
+print(list4.count("aa"))
+
+"""
+删除列表元素：
+"""
