8b97a5832903f053b9f1f194c57dbaa58691d62f
丁佳男 <jiananst@qq.com>
2025-04-28 01:36:08 +0800

    作用域

--- projects/hello_world/hello_world.py
+++ projects/hello_world/hello_world.py
@@ -30,3 +30,43 @@ def func3():
 func3()
 # 局部变量的作用：在函数体中临时保存数据，当函数被调用完之后，就会销毁局部变量
 # print("这是func3中local_value的值：", local_value)  # NameError: name 'local_value' is not defined
+
+
+"""
+当全局变量与局部变量命名重复时，全局变量的值并不会被局部变量的值影响而改变
+此时应该如何在函数内部修改全局变量的值？
+可以使用global关键字，global关键字的作用是：
+1.在函数内部声明全局变量
+2.在函数内部修改全局变量的值
+"""
+common_val = 100
+print("修改前的全局变量common_val的值：", common_val)
+
+
+def func4():
+    global common_val  # 声明全局变量
+    common_val = 200
+    print("这是func4中common_val的值：", common_val)
+
+
+func4()
+print("修改后的全局变量common_val的值：", common_val)
+
+
+def fun5():
+    global name
+    name = "Python基础"
+    print(f"正在学习的课程名称：{name}")
+
+
+fun5()
+print(name)
+
+
+def fun6():
+    print("获取fun5中定义的全局变量的值：", name)
+
+
+fun6()
+
+# 如何在函数体内部定义多个全局变量
