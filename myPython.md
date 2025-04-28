6092633922602cdb4c9879305a07dbb8df048a46
丁佳男 <jiananst@qq.com>
2025-04-28 23:40:17 +0800

    匿名函数

--- myPython.md
+++ myPython.md
@@ -2069,8 +2069,53 @@ print("全局变量a的值：", a)
 **调用格式**：结果 = 函数名 (实参)
 
 ```python
+"""
+匿名函数：函数名 = lambda 参数列表：表达式
+调用格式：结果 = 函数名(参数列表)
+"""
+# 1.求两个数的和
+# 普通函数
+def add(x, y):
+    return x + y
 
+print(add(1, 2))
+
+# 匿名函数
+# lambda函数不需要写return来返回值，表达式本身结果就是返回值
+lambda_add = lambda x, y: x + y
+print(lambda_add(1, 2))
+
+"""
+匿名函数的参数形式
+1.无参数
+2.一个参数
+3.默认参数
+4.可变参数
+5.关键字参数
+"""
+# 1.无参数
+no_param = lambda: 100
+print(no_param())
+
+# 2.一个参数
+one_param = lambda x: x + 1
+print(one_param(1))
+
+# 3.默认参数（默认参数必须写在非默认参数后面）
+default_param = lambda name, age=18: (name, age)
+print(default_param("张三"))
+print(default_param("李四", 20))
+
+# 4.可变参数
+var_param = lambda *args: args
+print(var_param(1, 2, 3, 4, 5))
+
+# 5.关键字参数
+key_param = lambda **kwargs: kwargs
+print(key_param(name="张三", age=18))
 ```
+
+https://www.bilibili.com/video/BV1rpWjevEip?spm_id_from=333.788.player.switch&vd_source=71b23ebd2cd9db8c137e17cdd381c618&p=22
 
 
 
--- projects/hello_world/hello_world.py
+++ projects/hello_world/hello_world.py
@@ -1,111 +1,44 @@
 """
-作用域：局部变量、全局变量
+匿名函数：函数名 = lambda 参数列表：表达式
+调用格式：结果 = 函数名(参数列表)
 """
-# 全局变量
-global_val = 100
+# 1.求两个数的和
+# 普通函数
+def add(x, y):
+    return x + y
 
+print(add(1, 2))
 
-def func1():
-    print("这是func1中global_val的值：", global_val)
-
-
-def func2():
-    global_val = 120
-    print("这是func2中global_val的值：", global_val)
-
-
-print("调用前global_val的值：", global_val)
-func1()
-func2()
-# global_val的值没有被覆盖是因为函数内部如果要使用变量，会先从函数内部寻找，有则直接调用，否则逐层向外寻找。
-print("调用后global_val的值：", global_val)
-
-
-# 局部变量
-def func3():
-    local_value = "Stone"
-    print("这是func3中local_value的值：", local_value)
-
-
-func3()
-# 局部变量的作用：在函数体中临时保存数据，当函数被调用完之后，就会销毁局部变量
-# print("这是func3中local_value的值：", local_value)  # NameError: name 'local_value' is not defined
-
+# 匿名函数
+# lambda函数不需要写return来返回值，表达式本身结果就是返回值
+lambda_add = lambda x, y: x + y
+print(lambda_add(1, 2))
 
 """
-当全局变量与局部变量命名重复时，全局变量的值并不会被局部变量的值影响而改变
-此时应该如何在函数内部修改全局变量的值？
-可以使用global关键字，global关键字的作用是：
-1.在函数内部声明全局变量
-2.在函数内部修改全局变量的值
+匿名函数的参数形式
+1.无参数
+2.一个参数
+3.默认参数
+4.可变参数
+5.关键字参数
 """
-common_val = 100
-print("修改前的全局变量common_val的值：", common_val)
+# 1.无参数
+no_param = lambda: 100
+print(no_param())
 
+# 2.一个参数
+one_param = lambda x: x + 1
+print(one_param(1))
 
-def func4():
-    global common_val  # 声明全局变量
-    common_val = 200
-    print("这是func4中common_val的值：", common_val)
+# 3.默认参数（默认参数必须写在非默认参数后面）
+default_param = lambda name, age=18: (name, age)
+print(default_param("张三"))
+print(default_param("李四", 20))
 
+# 4.可变参数
+var_param = lambda *args: args
+print(var_param(1, 2, 3, 4, 5))
 
-func4()
-print("修改后的全局变量common_val的值：", common_val)
-
-
-def fun5():
-    global name
-    name = "Python基础"
-    print(f"正在学习的课程名称：{name}")
-
-
-fun5()
-print(name)
-
-
-def fun6():
-    print("获取fun5中定义的全局变量的值：", name)
-
-
-fun6()
-
-
-# 如何在函数体内部定义多个全局变量：用英文逗号分隔
-def fun7():
-    global name1, name2
-    name1 = "Python基础"
-    name2 = "Python进阶"
-    print(f"正在学习的课程名称：{name1}和{name2}")
-
-
-fun7()
-print(name1, name2)
-
-"""
-nonlocal关键字：用来在嵌套函数中声明外层的局部变量
-注意：nonlocal只能对上一级嵌套函数中的变量进行修改
-"""
-a = 10
-
-
-def outer():
-    a = 5
-
-    def inner():
-        # nonlocal a  # 声明外层的局部变量
-        a = 20
-
-        def inner2():
-            nonlocal a  # 声明外层的局部变量
-            a = 30
-            print("inner2函数中a的值：", a)
-
-        inner2()
-        print("inner函数中a的值：", a)
-
-    inner()
-    print("outer函数中a的值：", a)
-
-
-outer()
-print("全局变量a的值：", a)
+# 5.关键字参数
+key_param = lambda **kwargs: kwargs
+print(key_param(name="张三", age=18))
