5e6bc541b1737678dd87f0333f75c010cb50c76e
丁佳男 <jiananst@qq.com>
2025-05-05 04:37:44 +0800

    闭包

--- myPython.md
+++ myPython.md
@@ -2577,7 +2577,75 @@ registry.reg('导入包后调用')
 
 
 
-#### 闭包
+### 32、闭包
+
+**定义**：在嵌套函数的前提下，内部函数使用了外部函数的变量，而且外部函数返回了内部函数，我们就把使用了外部函数变量的内部函数称为闭包。
+
+```python
+"""
+闭包：函数嵌套，内部函数引用外部函数的变量，并且外部函数返回内部函数的引用
+条件要素：
+    1.函数嵌套（函数中再定义函数）
+    2.内层函数调用外层函数的局部变量
+    3.外层函数的返回值是内层函数的函数名
+"""
+def outer():        # 外层函数
+    a = 10          # 外层函数的局部变量
+    def inner():    # 内层函数
+        print(a)    # 内层函数调用外层函数的局部变量
+    return inner    # 外层函数的返回值是内层函数的函数名
+
+# 调用方式一
+outer()()
+# 调用方式二
+f = outer()
+f()
+
+# 带参的闭包函数（外层函数的形参也是它的局部变量）
+def param_outer(m):
+    n = 10
+    def param_inner(o):
+        print(m + n + o)
+    return param_inner
+
+param_f = param_outer(5)
+param_f(20)
+
+# 闭包的原理：函数的引用
+def func_a():
+    print("func_a")
+print(func_a) # 函数名中保存了函数所在位置的引用（内存地址）
+
+# 每次开启内函数都在使用同一份闭包变量
+def outer_func(m):
+    print("outer_func()函数中的值：", m)
+    def inner_func(n):
+        print("inner_func()函数中的值：", m, n)
+        return m + n
+    return inner_func
+
+# 调用外函数，给outer_func()传值
+outer_f = outer_func(10)
+# 第一次调用内函数
+print(outer_f(20))
+# 第二次调用内函数
+print(outer_f(40))
+# 第三次调用内函数
+print(outer_f(60))
+# 总结：闭包函数可以记录外层函数的变量，并且可以多次调用
+```
+
+
+
+### 33、装饰器
+
+装饰器本质上是一个Python函数，它可以让其他函数在不需要做任何代码改动的前提下增加额外功能，装饰器的返回值也是一个函数对象。
+
+<span style="color:red;">装饰器需要满足以下两点：</span>
+
+1.不修改原程序或函数的代码
+
+2.不改变函数或程序调用方法
 
 
 
--- projects/hello_world/hello_world.py
+++ projects/hello_world/hello_world.py
@@ -1,16 +1,51 @@
-# 导入包的方式一
-# import my_package
-# # 调用包下的registry模块中的reg()函数
-# my_package.registry.reg('导入包后调用')
+"""
+闭包：函数嵌套，内部函数引用外部函数的变量，并且外部函数返回内部函数的引用
+条件要素：
+    1.函数嵌套（函数中再定义函数）
+    2.内层函数调用外层函数的局部变量
+    3.外层函数的返回值是内层函数的函数名
+"""
+def outer():        # 外层函数
+    a = 10          # 外层函数的局部变量
+    def inner():    # 内层函数
+        print(a)    # 内层函数调用外层函数的局部变量
+    return inner    # 外层函数的返回值是内层函数的函数名
 
-# 导入包的方式二
-# from my_package import registry
-# # 调用包下的registry模块中的reg()函数
-# registry.reg('导入包后调用')
+# 调用方式一
+outer()()
+# 调用方式二
+f = outer()
+f()
 
-# 导入包的方式三
-from my_package import *
-# 调用包下的registry模块中的reg()函数
-registry.reg('导入包后调用')
-# 无法调用login模块中的login()函数
-# login.login()
+# 带参的闭包函数（外层函数的形参也是它的局部变量）
+def param_outer(m):
+    n = 10
+    def param_inner(o):
+        print(m + n + o)
+    return param_inner
+
+param_f = param_outer(5)
+param_f(20)
+
+# 闭包的原理：函数的引用
+def func_a():
+    print("func_a")
+print(func_a) # 函数名中保存了函数所在位置的引用（内存地址）
+
+# 每次开启内函数都在使用同一份闭包变量
+def outer_func(m):
+    print("outer_func()函数中的值：", m)
+    def inner_func(n):
+        print("inner_func()函数中的值：", m, n)
+        return m + n
+    return inner_func
+
+# 调用外函数，给outer_func()传值
+outer_f = outer_func(10)
+# 第一次调用内函数
+print(outer_f(20))
+# 第二次调用内函数
+print(outer_f(40))
+# 第三次调用内函数
+print(outer_f(60))
+# 总结：闭包函数可以记录外层函数的变量，并且可以多次调用
